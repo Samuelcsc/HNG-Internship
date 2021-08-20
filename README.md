f955039dfe560e37fbe758f056636f0b58e21d3f
Sanda Adeola Samuel <sanda_adeola@yahoo.com>
2021-08-20 09:01:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HNG-Internship-8.0-Stage-2-Task
+ A pogram that prints my name
---
+++ index.js
@@ -0,0 +1,4 @@
+function myFunction(){
+    var x = "Adeola Sanda is my name";
+    document.querySelector(".child").innerHTML = x;
+}
